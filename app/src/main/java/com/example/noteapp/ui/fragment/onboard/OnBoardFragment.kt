95549be4d57e7ba662748fb9db71477138329397
Mik0ru <aziztermecikov5@gmail.com>
2024-11-19 03:44:23 +0600

    Added lottie

--- app/src/main/java/com/example/noteapp/ui/fragment/onboard/OnBoardFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragment/onboard/OnBoardFragment.kt
@@ -5,10 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import com.example.noteapp.R
 import com.example.noteapp.databinding.FragmentOnBoardBinding
 import com.example.noteapp.ui.adapters.OnBoardAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 
 class OnBoardFragment : Fragment() {
@@ -31,6 +33,7 @@ class OnBoardFragment : Fragment() {
 
     private fun initialise() {
         binding.viewpager.adapter = OnBoardAdapter(this)
+        TabLayoutMediator(binding.intoTabLayout, binding.viewpager) { _, _ -> }.attach()
     }
 
     private fun setupListeners() = with(binding.viewpager) {
@@ -39,7 +42,12 @@ class OnBoardFragment : Fragment() {
                 super.onPageSelected(position)
                 if (position == 2){
                     binding.txtSkip.visibility = View.INVISIBLE
+                    binding.btnStart.animate().translationY(0f).alpha(1f);
+                    binding.btnStart.setOnClickListener {
+                        findNavController().navigate(R.id.action_onBoardFragment_to_noteFragment)
+                    }
                 }else{
+                    binding.btnStart.animate().translationY(600f).alpha(0f)
                     binding.txtSkip.visibility = View.VISIBLE
                     binding.txtSkip.setOnClickListener {
                         if (currentItem < 3){
--- app/src/main/java/com/example/noteapp/ui/fragment/onboard/OnBoardPagerFragment.kt
+++ app/src/main/java/com/example/noteapp/ui/fragment/onboard/OnBoardPagerFragment.kt
@@ -31,14 +31,17 @@ class OnBoardPagerFragment : Fragment() {
             0 ->{
                 txtTitle.text = "Удобство"
                 txtBody.text = "Создавайте заметки в два клика! Записывайте мысли, идеи и важные задачи мгновенно."
+                lottie.setAnimationFromUrl("https://assets-v2.lottiefiles.com/tmp/optimize/22830e11-5d05-4d72-bf0b-610f76bdec1a/fvgizOW3Rm.json", "animation1")
             }
             1 ->{
                 txtTitle.text = "Организация"
                 txtBody.text = "Организуйте заметки по папкам и тегам. Легко находите нужную информацию в любое время."
+                lottie.setAnimationFromUrl("https://assets-v2.lottiefiles.com/tmp/optimize/65563416-76c8-4989-aadf-2a51dec5467b/YlZG5MginD.json", "animation2")
             }
             2 ->{
                 txtTitle.text = "Синхронизация"
                 txtBody.text = "Синхронизация на всех устройствах. Доступ к записям в любое время и в любом месте."
+                lottie.setAnimationFromUrl("https://assets-v2.lottiefiles.com/tmp/optimize/7cdf28fc-1a3c-4196-a509-b4b41e1aa8b0/V4K2ObwT44.json", "animation3")
             }
         }
     }
